cfeac8597bceb51c159e916d06175be973c422bc
iampraharsh <praharsh.tembhurne@gmail.com>
2025-11-19 19:18:16 +0530

    Admin page and quiz page

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,41 @@
+import React, { useEffect, useState } from 'react';
+import QuestionForm from '../components/QuestionForm';
+
+const Admin = () => {
+	const [quiz, setQuiz] = useState([]);
+
+	useEffect(() => {
+		const savedQuiz = JSON.parse(localStorage.getItem('quiz')) || [];
+		setQuiz(savedQuiz);
+	}, []);
+
+	const addQuestion = (newQ) => {
+		const updatedQuiz = [...quiz, newQ];
+
+		setQuiz(updatedQuiz);
+
+		localStorage.setItem('quiz', JSON.stringify(updatedQuiz));
+	};
+
+	return (
+		<div style={{ marginLeft: '20px' }}>
+			<h1>admin Panel</h1>
+
+			<QuestionForm onAdd={addQuestion} />
+
+			<h3>Saved Questions</h3>
+
+			{quiz.length === 0 && <p>No questions added yet.</p>}
+
+			{quiz.map((q, index) => (
+				<div key={index} style={{ marginBottom: '20px' }}>
+					<strong>
+						Q{index + 1}: {q.question}
+					</strong>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Admin;
---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,81 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Quiz = () => {
+	const [quiz, setQuiz] = useState([]);
+
+	const [currentQ, setCurrentQ] = useState(0);
+
+	const [selected, setSelected] = useState(null);
+
+	const [score, setScore] = useState(0);
+
+	const navigate = useNavigate();
+
+	useEffect(() => {
+		const savedQuiz = JSON.parse(localStorage.getItem('quiz')) || [];
+		setQuiz(savedQuiz);
+	}, []);
+
+	const handleSelect = (option) => {
+		setSelected(option);
+
+		if (option === quiz[currentQ].answer) {
+			setScore((s) => s + 1);
+		}
+	};
+
+	const handleNext = () => {
+		if (currentQ + 1 < quiz.length) {
+			setCurrentQ((q) => q + 1);
+			setSelected(null);
+		} else {
+			navigate('/result', { state: { score, total: quiz.length } });
+		}
+	};
+
+	if (quiz.length === 0) return <div>No quiz available</div>;
+
+	return (
+		<div>
+			<h2>Quiz</h2>
+
+			<h3>
+				Question {currentQ + 1}/{quiz.length}
+			</h3>
+
+			{/* show current Question  */}
+
+			<h3>{quiz[currentQ].question}</h3>
+
+			{/* button options */}
+			{quiz[currentQ].options.map((opt) => (
+				<button
+					key={opt}
+					disabled={selected !== null}
+					onClick={() => handleSelect(opt)}
+					style={{
+						marginRight: '10px',
+
+						background:
+							selected === opt
+								? selected === quiz[currentQ].answer
+									? 'lightgreen'
+									: 'lightcoral'
+								: '',
+					}}
+				>
+					{opt}
+				</button>
+			))}
+
+			{selected && (
+				<div style={{ marginTop: '20px' }}>
+					<button onClick={handleNext}>Next Question</button>
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default Quiz;
