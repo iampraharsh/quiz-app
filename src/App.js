a52e658d8d931fcea63ec82811b220b07f487c44
iampraharsh <praharsh.tembhurne@gmail.com>
2025-11-19 19:17:11 +0530

    Routing added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React from 'react';
-import './App.css';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import Quiz from './pages/Quiz';
+import Admin from './pages/Admin';
+import Result from './pages/Result';
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<BrowserRouter>
+			<div
+				style={{
+					marginLeft: '20px',
+				}}
+			>
+				<h1>Quiz App</h1>
+			</div>
+
+			<nav
+				style={{
+					display: 'flex',
+					gap: '30px',
+					marginBottom: '20px',
+					marginLeft: '20px',
+				}}
+			>
+				<Link to='/quiz'>Take Quiz</Link>
+				<Link to='/admin'>Admin Panel</Link>
+			</nav>
+
+			<Routes>
+				<Route path='/admin' element={<Admin />} />
+				<Route path='/quiz' element={<Quiz />} />
+				<Route path='/result' element={<Result />} />
+			</Routes>
+		</BrowserRouter>
+	);
 };
 
 export default App;
